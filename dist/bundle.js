d98343128dd4829719f375bba88630a5d973b691
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-05-09 18:46:29 -0400

    Revert ".gitignore init"
    This reverts commit 3b48a1a11e2e374d847092600c1078fe06e63f3c.

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************************!*\
+  !*** ./build-babel/Game/main.js ***!
+  \**********************************/
+
+
+/// <reference path="../Core/GameEngineTS.ts"/>
+console.log(Engine.Vector2.forward);
+//# sourceMappingURL=main.js.map
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
