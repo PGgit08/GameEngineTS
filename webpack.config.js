425a71c01a0dc83e828d8cf3e7eae9f0d250b27a
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-05-10 13:32:21 -0400

    setup webpack for mapped modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
+  resolve: {
+    plugins: [new TsConfigPathsPlugin()],
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
   devtool: "source-map",
   mode: "development",
   entry: './build-babel/Game/main.js',
@@ -8,4 +13,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  // Add the loader for .ts files.
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+      new CheckerPlugin()
+  ]
 };
