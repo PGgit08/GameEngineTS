ebef462794c768b6b788785a75b85f9420461619
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-05-18 11:52:34 -0400

    Made a low-level "drawable" class

---
+++ src/Core/Graphics/Drawable.ts
@@ -0,0 +1,40 @@
+import Vector2 from "@physics/Vector";
+
+/**
+ * Low-Level abstract class, that items which get drawn to the screen can inherit.
+ * @example Sprite Component  
+ */
+export default abstract class Drawable{
+    // point of rotation for drawable item
+    public origin: Vector2;
+
+    private _width: number = 0;
+    private _height: number = 0;
+
+    /**
+     * Get's width of IDrawable
+     */
+    public get width(): number{
+        return this._width;
+    };
+
+    public set width(w: number){
+        this._width = w;
+    };
+
+    /**
+     * Gets Height of IDrawable
+     */
+    public get height(): number{
+        return this._height;
+    };
+
+    public set height(h: number){
+        this._height = h;
+    };
+
+    /**
+     * Classes inheriting IDrawable override this abstract method.
+     */
+    protected abstract draw(x: number, y: number): void;
+};
