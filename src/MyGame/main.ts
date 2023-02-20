e8fc3012cf5bea706505749bc5f0386f1dca3460
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-02-19 20:20:35 -0500

    main.ts cleanup

--- src/MyGame/main.ts
+++ src/MyGame/main.ts
@@ -2,11 +2,8 @@ import {
     Engine,
 } from "../engine/GETS"
 
-const engine: Engine = new Engine(
+new Engine(
     (): void => { console.log("onStart() from main.ts!") }
 );
-
-engine.update();
-engine.render();
 
 export {}
