52613dc0767cf8d327831b08a40ddb3eacacd9cb
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-02-22 20:00:17 -0500

    started working on vector class

--- src/engine/core/math/Vector2.ts
+++ src/engine/core/math/Vector2.ts
@@ -1,3 +1,9 @@
 export class Vector2 {
-
+    private _x: number;
+    private _y: number;
+    
+    constructor(x: number = 0, y: number = 0) {
+        this._x = x;
+        this._y = y;
+    }
 }
