27e9778fa49cb3d6431db7e69d114c591d7304b5
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-03-07 21:35:13 -0500

    added color class

---
+++ src/engine/core/graphics/Color.ts
@@ -0,0 +1,34 @@
+import { vec4 } from "gl-matrix";
+
+export class Color {
+    public readonly r: number;
+    public readonly g: number;
+    public readonly b: number;
+    public readonly a: number;
+
+    public static readonly BLACK: Color = new Color(0, 0, 0);
+    public static readonly WHITE: Color = new Color(255, 255, 255);
+    public static readonly GREEN: Color = new Color(0, 255, 0);
+    public static readonly BLUE: Color = new Color(0, 0, 255);
+    public static readonly YELLOW: Color = new Color(255, 255, 0);
+    public static readonly RED: Color = new Color(255, 0, 0);
+    public static readonly ORANGE: Color = new Color(255, 128, 0);
+
+    /**
+     * Creates a new 8-bit RGBA Color.
+     * @param r The R value (8-bit).
+     * @param g The G value (8-bit).
+     * @param b The B value (8-bit).
+     * @param a The A value (8-bit). (default = 255)
+     */
+    constructor(r: number, g: number, b: number, a: number = 255) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+
+    public toVec4(): vec4 {
+        return vec4.fromValues(this.r / 255.0, this.g / 255.0, this.b / 255.0, this.a / 255.0);
+    }
+}
