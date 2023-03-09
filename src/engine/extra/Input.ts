bb1b6c132f52cc9bf2bb565e8c50181f26dc8b31
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-03-08 21:16:11 -0500

    fixed input issue

--- src/engine/extra/Input.ts
+++ src/engine/extra/Input.ts
@@ -1,23 +1,27 @@
 export class Input {
     private static _pressedKey: string = null;
-    private static _keyUp: boolean;
+    private static _newKey: boolean = null; // True if a the pressed key is new (or when reset)
 
 
     /**
      * Adds event listeners to the browser, should be called in the loading period.
      */
     public static addListeners() {
-        document.addEventListener("keyup", () => this._keyUp = true);
-        document.addEventListener("keydown", (e) => { this._keyUp = false; this._pressedKey = e.code; });
+        document.addEventListener("keyup", () => {
+            this._pressedKey = null;
+        });
+        
+        document.addEventListener("keydown", (e) => {
+            if (this._pressedKey !== e.code) { this._pressedKey = e.code; this._newKey = true; }
+        });
     }
 
     /**
-     * Returns true if the given key is pressed.
+     * Returns true if the given key is pressed during the frame.
      */
     public static KeyPressed(key: string): boolean {
-        if (this._pressedKey === key) {
-            if (this._keyUp) { this._pressedKey = null; }
-            
+        if (this._pressedKey === key && this._newKey) {
+            this._newKey = false;         
             return true;
         } else { return false; }
     }
