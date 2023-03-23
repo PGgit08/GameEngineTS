a7c8b9abd1f319a1824dd727176b05c101e79c2d
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-03-22 21:42:51 -0400

    messing around with textures

--- src/SnakeGame/entities/Food.ts
+++ src/SnakeGame/entities/Food.ts
@@ -8,6 +8,6 @@ export class Food extends Entity {
         this.transform.scale = vec2.fromValues(0.15, 0.15);
         // this.transform.position = 
 
-        this.addComponents(new MeshComponent(new Mesh(new Square(), new StandardMaterial(undefined, Color.RED))));
+        this.addComponents(new MeshComponent(new Mesh(new Square(), new StandardMaterial(undefined, undefined))));
     }
 }
