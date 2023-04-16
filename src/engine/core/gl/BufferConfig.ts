29648a13ed34477107e6e0448fc18b9159eb9fe7
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-04-16 18:23:11 -0400

    comments

--- src/engine/core/gl/BufferConfig.ts
+++ src/engine/core/gl/BufferConfig.ts
@@ -1,3 +1,6 @@
+/**
+ * Contains the names of Buffers in Geometries.
+ */
 export class BufferConfig {
     public static readonly POSITION_BUFFER_NAME = "POSITION_BUFFER";
     public static readonly TEXTURE_BUFFER_NAME = "TEXTURE_BUFFER";
