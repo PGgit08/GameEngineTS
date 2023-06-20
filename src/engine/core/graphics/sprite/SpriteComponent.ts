70483d438f796fa8724986c47b024679c7e6b856
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-06-20 19:33:33 -0400

    started working on sprite component layering, realized events are not required to solve issue

--- src/engine/core/graphics/sprite/SpriteComponent.ts
+++ src/engine/core/graphics/sprite/SpriteComponent.ts
@@ -1,3 +1,4 @@
+import Dictionary from "../../../extra/Dictionary";
 import { Component } from "../../ecs/Component";
 import { RendererManager } from "../../managers/RendererManager";
 import { SceneManager } from "../../managers/SceneManager";
@@ -7,6 +8,53 @@ export class SpriteComponent extends Component {
     private _sprite: Sprite;
     public visible: boolean = true;
     
+    private _layer: string;
+    private _layerOrder: number;
+
+    private static _layers: Dictionary<string, Sprite[]> = {
+        "Default": []
+    };
+
+    public set layer(layer: string) {
+        if (this.owner.parentScene !== SceneManager.getInstance().currentScene) {
+            return;
+        }
+
+        // Removes the Sprite from its current layer at its current order
+        SpriteComponent._layers[this._layer].splice(this._layerOrder, 1);
+
+        if (this._layerOrder < SpriteComponent._layers[layer].length) {
+            // if the current layer order works for the new layer, use it
+            SpriteComponent._layers[layer].splice(this._layerOrder, 0, this._sprite);
+        } else {
+            // if the current layer order does not work for the new layer, push it to the end of the new layer
+            SpriteComponent._layers[layer].push(this._sprite);
+            this._layerOrder = SpriteComponent._layers[layer].length - 1;
+        }
+
+        this._layer = layer;
+    }
+
+    public set layerOrder(layerOrder: number) {
+        if (this.owner.parentScene !== SceneManager.getInstance().currentScene) {
+            return;
+        }
+
+        if (layerOrder < SpriteComponent._layers[this._layer].length) {
+            // if the new layer order works for the current layer, use it
+            SpriteComponent._layers[this._layer].splice(layerOrder, 0, this._sprite);
+            this._layerOrder = layerOrder;
+        }
+    }
+
+    public get layer(): string {
+        return this._layer;
+    }
+
+    public get layerOrder(): number {
+        return this._layerOrder;
+    }
+
     public get sprite(): Sprite {
         return this._sprite;
     }
@@ -15,6 +63,7 @@ export class SpriteComponent extends Component {
         super("SpriteComponent");
 
         this._sprite = sprite;
+        this._layer = "Default";
     }
 
     public load(): void {
