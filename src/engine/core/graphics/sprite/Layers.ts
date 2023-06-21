d706c2aa8ab2e06368ba672591c81ad821a0a460
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-06-21 10:32:29 -0400

    created layers class

---
+++ src/engine/core/graphics/sprite/Layers.ts
@@ -0,0 +1,10 @@
+import Dictionary from "../../../extra/Dictionary";
+import { Sprite } from "./Sprite";
+
+export class Layers {
+    private _layers: Dictionary<string, Sprite[]> = {};
+
+    constructor() {
+        
+    }
+}
