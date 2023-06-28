7acdc7306287f2a6135d14f525be91494612b419
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-06-28 11:15:14 -0400

    clean up

--- src/TestGame/main.ts
+++ src/TestGame/main.ts
@@ -15,10 +15,6 @@ new Engine(
         scenes: [MyScene],
         
         textures: [
-            // {name: 'f-texture', fileName: new URL('./assets/f-texture.png', import.meta.url).href},
-            // {name: 'emoji', fileName: new URL('./assets/emoji.jpg', import.meta.url).href},
-            // {name: 'sprite1', fileName: new URL('./assets/Sprite1.png', import.meta.url).href},
-            // {name: 'testJson', fileName: new URL('./assets/testJson.png', import.meta.url).href, configJson: testJson}
             {name: 'ArmsAnimation', fileName: new URL('./assets/ArmsAnimation.png', import.meta.url).href, configJson: ArmsAnimation},
             {name: 'Crosshair', fileName: new URL('./assets/Crosshair.png', import.meta.url).href},
             {name: 'FTEXT', fileName: new URL('./assets/f-texture.png', import.meta.url).href}
