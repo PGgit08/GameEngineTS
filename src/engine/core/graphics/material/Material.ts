41945a25e47d9fd4815d6632a6904df933c89822
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-07-08 11:17:00 -0400

    jsdoc'd materials

--- src/engine/core/graphics/material/Material.ts
+++ src/engine/core/graphics/material/Material.ts
@@ -5,6 +5,26 @@ import { Color } from "../Color";
 import { Texture } from "../Texture";
 import { GameObject } from "../../ecs/GameObject";
 
+import { Mesh } from "../Mesh";
+import { Geometry } from "../geometry/Geometry";
+import { ShaderConfig } from "../../config/ShaderConfig";
+
+
+/**
+ * @classdesc
+ * The second part making up a {@link Mesh}. This object "paints" a {@link Geometry}. It applies all standard uniforms on the Geometry
+ * (model mat, projection mat, view mat, texture, color) and can also apply any additional uniforms on the Geometry. It is meant to be
+ * override by any custom Materials.
+ * 
+ * @class Material
+ * @extends GameObject
+ * @abstract
+ * 
+ * @param {string} name - The name of this Material.
+ * @param {string} shaderName - The name of the {@link Shader} used by this Material.
+ * @param {Texture} [texture] - The Texture used by this Material (DEFAULT IS WHITE).
+ * @param {Color} [color] - The Color used by this Material (DEFAULT IS BLACK).
+ */
 export abstract class Material extends GameObject {
     protected _shader: Shader;
 
@@ -27,13 +47,22 @@ export abstract class Material extends GameObject {
         }
     }
 
+    /**
+     * Applies the standard uniforms (described in {@link ShaderConfig}) on the Geometry.
+     * 
+     * @param {mat3} model - The model matrix.
+     * @param {mat3} projection - The projection matrix.
+     * @param {mat3} view - The view matrix.
+     */
     public applyStandardUniforms(model: mat3, projection: mat3, view: mat3): void {
         this._shader.use();
         this._shader.applyStandardUniforms(model, projection, view, this.texture, this.color);
     }
 
     /**
-     * This can be used by the Material to apply any additional Shader uniforms.
+     * This can be used by a custom Material to apply any additional Shader uniforms on the Geometry.
+     * 
+     * @abstract
      */
     public abstract applyAdditionalUniforms(): void;
 }
--- src/engine/core/graphics/material/StandardMaterial.ts
+++ src/engine/core/graphics/material/StandardMaterial.ts
@@ -2,14 +2,24 @@ import { Color } from "../Color";
 import { Texture } from "../Texture";
 import { Material } from "./Material";
 
+import { ShaderConfig } from "../../config/ShaderConfig";
+import { StandardShader } from "../../gl/shader/StandardShader";
+
 /**
- * Represents a Standard Material with ONLY the required attribs/uniforms.
- * This Material uses the built-in Standard Shader. 
+ * @classdesc
+ * Represents a Standard Material with ONLY the required attribs/uniforms described in {@link ShaderConfig}.
+ * This Material uses the built-in {@link StandardShader}.
+ * 
+ * @class StandardMaterial
+ * @extends Material
+ * 
+ * @param {Texture} [texture] - The Texture of this Material (DEFAULT IS WHITE).
+ * @param {Color} [color] - The Color of this Material (DEFAULT IS BLACK).
  */
 export class StandardMaterial extends Material {
     constructor(texture?: Texture, color?: Color) {
         super("StandardMaterial", "StandardShader", texture, color);
     }
 
-    public applyAdditionalUniforms(): void {}
+    public applyAdditionalUniforms(): void {} // empty, nothing custom created
 }
