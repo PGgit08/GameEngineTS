fe9661e48d58387dacfb5be4d65e0709b44bcbc4
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-08-07 16:13:10 -0400

    removed jest to try another testing solution

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    transform: {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-
-    roots: [
-      "./tests"
-    ]
-}
--- tests/test1.test.ts
+++
@@ -1,5 +0,0 @@
-test("Test 1", () => {
-    expect(typeof "hello").toBe("string");
-});
-
-export {}
