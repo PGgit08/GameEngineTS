983b51ccb5774f5159e20e9750e021064ae05917
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-11-26 19:50:56 -0500

    layer issue works?

--- src/FlappyBird/entities/GameBackground.ts
+++ src/FlappyBird/entities/GameBackground.ts
@@ -1,4 +1,4 @@
-import { Background, Entity } from "../../engine/GETS";
+import { Background, Entity, SpriteComponent } from "../../engine/GETS";
 import { BackgroundBehavior } from "../behaviors/BackgroundBehavior";
 
 export class GameBackground extends Entity {
@@ -6,12 +6,18 @@ export class GameBackground extends Entity {
         super("GameBackground");
 
         const ground = new Background("Ground", "ground");
+        
         ground.transform.position[1] = 270;
         ground.transform.scale[1] = 2;
+        ground.getComponent(SpriteComponent).layerOrder = 0;
+
+        const city = new Background("City", "background");
+        city.getComponent(SpriteComponent).layerOrder = 1;
+
 
         this.addChildren(
             ground,
-            new Background("City", "background")
+            city
         )
 
         this.addComponents(new BackgroundBehavior());
